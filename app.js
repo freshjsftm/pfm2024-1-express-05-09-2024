80e4b038d6a84a23cd9ff0edbcf6d954e1a281b9
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-05 10:00:24 +0300

    Add pattern *

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const app = express();
 const PORT = 3000;
 
 // ROUTING
-app.get('/')
+app.get('/', (req, res) => {
+  res.send(`<h1>Home! method - ${req.method}, url - ${req.path}</h1>`);
+});
+app.get('*', (req, res) => {
+  res.send(`<h1>404 ! method - ${req.method}, url - ${req.path}</h1>`);
+});
 // app.post()
 // app.put()
 // app.delete()
 
-app.listen(PORT, ()=>{
-  console.log('app started at port '+PORT);
-  
+app.listen(PORT, () => {
+  console.log('app started at port ' + PORT);
 });
