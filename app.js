e240f191475cbff3ee585614f1b1b6b29a627014
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-05 10:19:42 +0300

    Add example middleware

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ const app = express();
 const PORT = 3000;
 
 // ROUTING
-app.get('/', (req, res) => {
-  res.send(`<h1>Home! method - ${req.method}, url - ${req.path}</h1>`);
-});
-app.get('*', (req, res) => {
-  res.send(`<h1>404 ! method - ${req.method}, url - ${req.path}</h1>`);
-});
+app.get(
+  '/',
+  (req, res, next) => {
+    req.test = 10;
+    console.log('1');
+    next();
+  },
+  (req, res, next) => {
+    req.test +=5;
+    console.log('2');
+    next();
+  },
+  (req, res) => {
+    res.send('hi, req.test = '+req.test);
+  }
+);
+
+// app.get('*', (req, res) => {
+//   res.send(`<h1>404 ! method - ${req.method}, url - ${req.path}</h1>`);
+// });
 // app.post()
 // app.put()
 // app.delete()
